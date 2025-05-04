c4d3c913135d24286e1b537aa8a8950f9e1dcc20
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2025-05-04 23:16:49 +0530

    Make header index

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ function Header({ token, logout, role }) {
   };
 
   return (
-    <nav className="bg-primary-800 text-white shadow-lg fixed top-0 left-0 right-0">
+    <nav className="bg-primary-800 text-white shadow-lg fixed top-0 left-0 right-0 z-10">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold">
           CountryAPI Application
